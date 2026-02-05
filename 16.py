855e41d309e8206106ad1faf7a233e3360ef86c3
Nishad9060 <SBMPC.Student@SVKMGRP.COM>
2026-02-05 08:37:33 +0530

    Nishad

---
+++ 16.py
@@ -0,0 +1,4 @@
+dict = {'Name': 'hitesh', 'Age': 7, 'Class': 'First'}
+del dict['Name']; # remove entry with key 'Name'
+dict.clear(); # remove all entries in dict
+del dict ; # delete entire dictionary
---
+++ 17.py
@@ -0,0 +1,3 @@
+dict = {'Name': 'hitesh', 'Age': 7, 'Class': 'First'}
+for key, value in dict.items():
+    print(key, ' - ', value)
